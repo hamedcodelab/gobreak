9441698c6a5d579010ccd0d3564880ca166bacae
hamedcodelab <en.hamedamini@gmail.com>
2025-04-04 20:21:21 +0330

    Refactor circuit breaker initialization and structure.

--- interface.go
+++ interface.go
@@ -14,47 +14,32 @@ type Breaker interface {
 // mu: A mutex to ensure thread-safe access to the circuit breaker.
 // state: The current state of the circuit breaker (Closed, Open, or HalfOpen).
 // failureCount: The current count of consecutive(successive) failures.
-// failureThresholdAllow: The number of consecutive failures allowed before opening the circuit.
+// failureThresholdAllow: The number of consecutive failures allowed before opening and half opening the circuit.
 // recoveryTimeToHalfOpen: The cool-down period before the circuit breaker transitions from Open to HalfOpen.
 // halfOpenMaxRequests: The maximum number of successful requests needed to close the circuit.
 
-/*
-// lastFailureTime: The timestamp of the last failure.
-// halfOpenSuccessCount: The number of successful requests in the HalfOpen state.
-// timeout: The maximum duration to wait for a request to complete.
-*/
 type breaker struct {
-	mu                  sync.Mutex
-	state               State
-	failureCount        int
-	SuccessRequestCount int
-	//lastFailureTime      time.Time
-	//halfOpenSuccessCount int
+	mu                     sync.Mutex
+	state                  State
+	failureCount           int
+	SuccessRequestCount    int
 	failureThresholdAllow  int
 	recoveryTimeToHalfOpen time.Duration
 	timeToHalfOpen         time.Time
 	halfOpenMaxRequests    int
-	//timeout              time.Duration
 }
 
 func NewBreaker(opts ...Option) Breaker {
 	b := &breaker{
-		state: Closed,
-	}
-	for _, opt := range opts {
-		opt(b)
+		failureCount:           0,
+		recoveryTimeToHalfOpen: 10 * time.Second,
+		halfOpenMaxRequests:    1,
+		failureThresholdAllow:  3,
+		state:                  Closed,
 	}
 
-	if b.failureCount == 0 {
-		b.failureCount = 1
-	}
-	if b.recoveryTimeToHalfOpen == 0 {
-		b.recoveryTimeToHalfOpen = 10 * time.Second
-	}
-	if b.halfOpenMaxRequests == 0 {
-		b.halfOpenMaxRequests = 1
-	}
-	if b.failureThresholdAllow == 0 {
+	for _, opt := range opts {
+		opt(b)
 	}
 	return b
 }
